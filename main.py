44a32c9dc5c616688dc0bfe293e2f0a6de8012b5
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-02-27 10:43:46 -0300

    inicial

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
-import colorgram
+# ###This code will not work in repl.it as there is no access to the colorgram package here.###
+# ##We talk about this in the video tutorials##
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract("image.jpg", 30)
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    rgb_colors.append(color.rgb)
+import random
+import turtle as t
 
-print(rgb_colors)
+tim = t.Turtle()
+screen = t.Screen()
+tim.penup()
+tim.setposition(-250, -250)
+color_list = [(245, 243, 238), (246, 242, 244), (202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+tim.shape("arrow")
+t.colormode(255)
+
+for i in range(10):
+    for _ in range(10):
+        color = random.choice(color_list)
+        tim.pendown()
+        tim.dot(20, color)
+        tim.penup()
+        tim.forward(50)
+
+    tim.left(90)
+    tim.forward(50)
+    tim.left(90)
+    for _ in range(10):
+        tim.forward(50)
+    tim.left(180)
+
+
+screen.exitonclick()
